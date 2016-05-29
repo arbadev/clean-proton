ed0b3db38e722e89ef788c36eb4083595debb5f4
Andres Barradas <a3barradas@gmail.com>
2016-05-29 16:23:03 -0400

    Aggregate update method on spark model

--- api/controllers/SparkController.js
+++ api/controllers/SparkController.js
@@ -9,11 +9,20 @@ export default class SparkController extends Controller {
     try {
       this.body = Spark.create(this.request.body)
       this.status = 201
-
     } catch (err) {
       proton.log.error('SparkController.create', err)
       this.status = 400
     }
   }
 
+  * update() {
+    proton.log.debug('update spark : ', this.request.body)
+    try {
+      this.body = Spark.update(this.request.body)
+      this.status = 201
+    } catch (err) {
+      proton.log.error('SparkController.update', err)
+      this.status = 400
+    }
+  }
 }
--- api/models/Spark.js
+++ api/models/Spark.js
@@ -1,4 +1,4 @@
-'use strict'
+  'use strict'
 
 import Model from 'proton-mongoose-model'
 import _ from 'lodash'
@@ -28,8 +28,13 @@ export default class Spark extends Model {
   }
 
   /**
-   * @description
+   * @description this method gets the params (from , to), and find if a Spark
+   alredy exist with those users, if exist it will update the data with their
+   answers, otherwise create a new spark with the undefined status waiting for
+   the other User asnwer
    * @author Andres Barradas <andres@nucleos.io>
+   * @param from: this always will be the client who ask for a relationship
+   * @param to: is the user which its asked for a relationship
    */
   static * create({ from, to }) {
     const { decision } = from.levels.one
@@ -48,6 +53,46 @@ export default class Spark extends Model {
     const status = decision ? 'undefined' : 'terminated'
     spark = new this({ from, to, status })
     return spark.save()
+  }
+
+  /**
+   * @description this method gets the params (from , to), and find a spark where
+   the users are and update their relationship
+   * @author Andres Barradas <andres@nucleos.io>
+   * @param from: this always will be the client which want to start the next lv
+   * @param level: {two: { decision: true, message: 'url' } }
+   */
+
+  static * updateLevel(id, userId, level) {
+    const criteria = { _id: new Spark.types.ObjectId(id) }
+    const spark = yield Spark.findOne(criteria)
+    const sparkMate = spark.from.user == userId : spark.to ? spark.from
+    const key = spark.from.user == userId : 'from' ? 'to'
+    const [levelName] = Object.getOwnPropertyNames(level)
+    const decision = level[levelName].decision
+    // let status = !decision ? 'terminated' : sparkMate.levels[levelName] && sparkMate.levels[levelName].decision ? levelName : spark.status
+
+    if (!decision) {
+      let status = 'terminated'
+    }else if (sparkMate.levels[levelName] && sparkMate.levels[levelName].decision) {
+      status = levelName
+    }else {
+      status = spark.status
+    }
+
+    const key = spark.from.user == userId ? 'from' : 'to'
+    const updateData = {
+      status,
+      `${key}.levels`: Object.assign({}, spark[key].levels, level),
+    }
+
+    const updatedSpark = yield Spark.findOneAndUpdate(criteria, updateData, { new: true })
+    const pushMessage = {
+      event: 'updateMessage',
+      data: { _id: id }
+    }
+    NotificationService.sendPush(sparkMate.user, pushMessage)
+    return return Promise.resolve(updatedSpark)
   }
 
 }
