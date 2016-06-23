f4c81306ff5137d4a5a45d195c1351d63e13a2eb
Carlos Marcano <cmarcano631@gmail.com>
2016-06-23 00:29:34 -0400

    Require dotenv only in development mode

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 require('babel-polyfill')
 require('babel-core/register')
-require('dotenv').config();
 
 const Proton = require('proton-koa')
 const api = require('./api')
 const app = new Proton(api)
+
+if (app.env === 'development') {
+  console.log('================= Server in development mode =================\n')
+  require('dotenv').config()
+}
 
 app.use(function * (next) {
   const requestInfo = `${this.request.method} ${this.request.url}`
@@ -14,4 +18,4 @@ app.use(function * (next) {
   yield next
 })
 
-module.exports =  app.start()
+module.exports = app.start()
