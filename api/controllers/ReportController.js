94ee2ae81850cb8cd90566e94ab007156fccbff0
Carlos Marcano <cmarcano631@gmail.com>
2016-07-14 20:44:34 -0400

    Create a user report

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -6,6 +6,10 @@ export default class ReportController extends Controller {
 
   * create() {
     try {
+      const from = this.request.user._id
+      const to = this.params.userId
+      const { reason } = this.request.body
+      yield Report.create({ from, to, reason })
       this.response.status = 204
     } catch (err) {
       proton.log.error('ReportController.create', err)
