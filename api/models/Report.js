083c7b866b15c4fdf6b78949e5fcbb0fc1a35071
Andres Barradas <arbadev@gmail.com>
2016-08-30 18:04:56 -0400

    added enum variables in report model

--- api/models/Report.js
+++ api/models/Report.js
@@ -6,7 +6,11 @@ export default class Report extends Model {
 
   schema() {
     return {
-      reason: String,
+      reason: {
+        type: String,
+        enum: ['Aggression', 'Verbal abuse', 'Sexual harassment'],
+        required: true,
+      },
       description: String,
       from: {
         type: Model.types.ObjectId,
