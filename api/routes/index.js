5ebfdb27d6e392b98251cb91b3c071d769e3a415
Carlos Marcano <cmarcano631@gmail.com>
2016-09-24 01:05:18 -0400

    create test for level 2

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,3 +3,27 @@ const router = new Router()
 router.get('/', function * () {
   this.body = 'Welcome to persona app'
 })
+
+
+router.post('/test-level-2', function * () {
+  const andres = yield User.findOne({ email: 'cjose.m@hotmail.com' })
+  const marian = yield User.findOne({ email: 'msalcedo047@gmail.com' })
+  const mToA = {
+    value: 'like',
+    from: marian._id,
+    to: andres._id,
+  }
+  const aToM = {
+    value: 'like',
+    from: andres._id,
+    to: marian._id,
+  }
+
+  // create sparkd
+  yield Like.create(mToA)
+  yield Like.create(aToM)
+
+    // update to level 2
+  yield Like.create(mToA)
+  yield Like.create(aToM)
+})
