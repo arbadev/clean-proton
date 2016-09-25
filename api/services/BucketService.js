7c3bc7fb4307a3a8fd6312ef429837e2f1ad2dd6
Carlos Marcano <cmarcano631@gmail.com>
2016-09-25 13:47:36 -0400

    Revert changes

--- api/services/BucketService.js
+++ api/services/BucketService.js
@@ -4,14 +4,16 @@ import Service from 'proton-service'
 import hat from 'hat'
 import aws from 'aws-sdk'
 
-aws.config.update({ region: 'us-east-1' })
 const bucket = process.env.AWS_BUCKET_NAME
 
 export default class BucketService extends Service {
 
   generateBucketObject() {
     const name = hat()
-    const type = ''
+    const type = 'audio/mpeg'
+    aws.config.update({
+      region: 'us-east-1',
+    })
     const s3 = new aws.S3()
     const params = {
       Bucket: bucket,
@@ -20,7 +22,8 @@ export default class BucketService extends Service {
       ContentType: type,
       ACL: 'public-read',
     }
-    const signedUri = s3.getSignedUrl('putObject', params)
+    const data = s3.getSignedUrl('putObject', params)
+    const signedUri = data
     const uri = `https://${bucket}.s3.amazonaws.com/${name}`
     return { signedUri, uri }
   }
