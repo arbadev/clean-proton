c48c7bb25d2e669d98b5639051eb51b9de3aa87c
Carlos Marcano <cmarcano631@gmail.com>
2016-09-25 13:30:19 -0400

    Added .mp3 extension to s3 files

--- api/services/BucketService.js
+++ api/services/BucketService.js
@@ -4,16 +4,14 @@ import Service from 'proton-service'
 import hat from 'hat'
 import aws from 'aws-sdk'
 
+aws.config.update({ region: 'us-east-1' })
 const bucket = process.env.AWS_BUCKET_NAME
 
 export default class BucketService extends Service {
 
   generateBucketObject() {
-    const name = hat()
+    const name = `${hat()}.mp3`
     const type = 'audio/mpeg'
-    aws.config.update({
-      region: 'us-east-1',
-    })
     const s3 = new aws.S3()
     const params = {
       Bucket: bucket,
@@ -22,8 +20,7 @@ export default class BucketService extends Service {
       ContentType: type,
       ACL: 'public-read',
     }
-    const data = s3.getSignedUrl('putObject', params)
-    const signedUri = data
+    const signedUri = s3.getSignedUrl('putObject', params)
     const uri = `https://${bucket}.s3.amazonaws.com/${name}`
     return { signedUri, uri }
   }
