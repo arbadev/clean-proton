aae8269170d6dd891a48398923dc7be9acc037a9
Carlos Marcano <cmarcano631@gmail.com>
2016-09-25 15:01:06 -0400

    revert changes

--- api/services/BucketService.js
+++ api/services/BucketService.js
@@ -9,8 +9,8 @@ const bucket = process.env.AWS_BUCKET_NAME
 export default class BucketService extends Service {
 
   generateBucketObject() {
-    const name = `${hat()}.mp3`
-    const type = 'audio/mpeg3'
+    const name = hat()
+    const type = 'audio/mpeg'
     aws.config.update({
       region: 'us-east-1',
     })
@@ -22,7 +22,8 @@ export default class BucketService extends Service {
       ContentType: type,
       ACL: 'public-read',
     }
-    const signedUri = s3.getSignedUrl('putObject', params)
+    const data = s3.getSignedUrl('putObject', params)
+    const signedUri = data
     const uri = `https://${bucket}.s3.amazonaws.com/${name}`
     return { signedUri, uri }
   }
