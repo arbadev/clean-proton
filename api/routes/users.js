943200f29b1af95d413c5b5857212536c92deff9
Andres Barradas <arbadev@gmail.com>
2016-10-03 16:10:55 -0400

    clean proton commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,31 +3,10 @@ import Router from 'koa-router'
 
 const router = new Router({ prefix: '/users' })
 const {
-  UserController,
-  ReportController,
+  UserController
 } = proton.app.controllers
-//
-// router.get('/', AuthPolicies.bearer, UserController.find)
-// router.post('/', AuthPolicies.bearerWithoutUser, UserController.create)
-//
-// router.put('/me', AuthPolicies.bearer, UserController.updateMe)
-//
-// router.get('/me', AuthPolicies.bearer, UserController.findMe)
-// router.patch('/me', AuthPolicies.bearer, UserController.updateMe)
-//
-// router.delete('/:userId', UserController.destroy)
-//
-// router.post('/:id/like', AuthPolicies.bearer, UserController.like)
-//
-// router.post('/:id/dislike', AuthPolicies.bearer, UserController.dislike)
-//
-// /*   users reports  */
-// router.post('/:userId/report', AuthPolicies.bearer, ReportController.create)
-//
-// /*   users feedbacks  */
-// router.post('/feedback', AuthPolicies.bearer, FeedbackController.create)
 
-/*    test EmailService   */
-// router.post('/mail', ReportController.emailTest)
+// router.get('/', AuthPolicies.bearer, UserController.find)
+
 
 module.exports = router
